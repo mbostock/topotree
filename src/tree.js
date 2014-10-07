6988f952822acf3da3838a94cd30727b5ec99c41
Mike Bostock <mbostock@gmail.com>
2014-10-07 10:52:07 -0700

    Shorter code.

--- src/tree.js
+++ src/tree.js
@@ -24,24 +24,12 @@ function tree_group(children) {
   var i0,
       i1,
       n0,
-      n1,
-      child0,
-      child1,
       children1;
 
   while ((n0 = children.length) > 1) {
-    children1 = new Array(n1 = Math.ceil(n0 / 2));
-
-    for (i0 = 0, i1 = 0; i0 < n0 - 1; i0 += 2, i1 += 1) {
-      child0 = children[i0];
-      child1 = children[i0 + 1];
-      children1[i1] = new Node(child0, child1);
-    }
-
-    if (i0 < n0) {
-      children1[i1] = children[i0];
-    }
-
+    children1 = new Array(Math.ceil(n0 / 2));
+    for (i0 = 0, i1 = 0; i0 < n0 - 1; i0 += 2, i1 += 1) children1[i1] = new Node(children[i0], children[i0 + 1]);
+    if (i0 < n0) children1[i1] = children[i0];
     children = children1;
   }
 
