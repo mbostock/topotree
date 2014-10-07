360df229ccba8f61a39a859f94f2c1f8a2232a9e
Mike Bostock <mbostock@gmail.com>
2014-10-07 16:47:13 -0700

    Remove dead code.

--- src/ccw.js
+++
@@ -1,6 +0,0 @@
-// Three points are a counter-clockwise turn if ccw > 0, clockwise if ccw < 0,
-// and collinear if ccw = 0 because ccw is a determinant that gives twice the
-// signed area of the triangle ABC. Assumes the origin [0,0] is the top-left.
-function ccw(a, b, c) {
-  return (b[1] - a[1]) * (c[0] - a[0]) - (b[0] - a[0]) * (c[1] - a[1]);
-}
