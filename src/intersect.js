daf923c3d8306bb37d24a46306ac0f003b3be409
Mike Bostock <mbostock@gmail.com>
2014-10-06 17:12:46 -0700

    Reformat.

--- src/intersect.js
+++ src/intersect.js
@@ -1,39 +1,43 @@
 import "ccw";
 
-// Returns true if the bounding box AB intersects the line segment CD
+// Returns true if the bounding box AB intersects the line segment CD.
 // Note: assumes that a[0] <= b[0] and a[1] <= b[1].
 function intersectBoxSegment(a, b, c, d) {
-  var s, t;
-  return !(c[0] > b[0] && d[0] > b[0]
-        || c[0] < a[0] && d[0] < a[0]
-        || c[1] > b[1] && d[1] > b[1]
-        || c[1] < a[1] && d[1] < a[1])
+  var a0 = a[0], a1 = a[1],
+      b0 = b[0], b1 = b[1],
+      c0 = c[0], c1 = c[1],
+      d0 = d[0], d1 = d[1],
+      s, t;
+
+  // Computing bounding box of CD.
+  if (c0 > d0) s = c0, c0 = d0, d0 = s;
+  if (c1 > d1) s = c1, c1 = d1, d1 = s;
+
+  return !(c0 > b0 || d0 < a0 || c1 > b1 || d1 < a1)
       && (!(s = ccw(c, d, a))
-        || !(t = ccw(c, d, b)) || t > 0 !== (s = s > 0)
-        || !(t = ccw(c, d, [b[0], a[1]])) || t > 0 !== s
-        || !(t = ccw(c, d, [a[0], b[1]])) || t > 0 !== s);
+        || !(t = ccw(c, d, b))
+        || t > 0 !== (s = s > 0)
+        || !(t = ccw(c, d, [b0, a1]))
+        || t > 0 !== s
+        || !(t = ccw(c, d, [a0, b1]))
+        || t > 0 !== s);
 }
 
-// Returns true if the line segments AB and CD intersect
-// TODO: Assume overlapping bounding box so as to avoid recomputing it?
+// Returns true if the line segments AB and CD intersect.
 function intersectSegmentSegment(a, b, c, d) {
-  var a0 = a[0],
-      a1 = a[1],
-      b0 = b[0],
-      b1 = b[1],
+  var a0 = a[0], a1 = a[1],
+      b0 = b[0], b1 = b[1],
+      c0 = c[0], c1 = c[1],
+      d0 = d[0], d1 = d[1],
       s;
 
-  // Determine bounding box.
+  // Computing bounding box of AB and CD.
   if (a0 > b0) s = a0, a0 = b0, b0 = s;
   if (a1 > b1) s = a1, a1 = b1, b1 = s;
+  if (c0 > d0) s = c0, c0 = d0, d0 = s;
+  if (c1 > d1) s = c1, c1 = d1, d1 = s;
 
-  // Bounding box check.
-  if (c[0] > b0 && d[0] > b0
-      || c[0] < a0 && d[0] < a0
-      || c[1] > b1 && d[1] > b1
-      || c[1] < a1 && d[1] < a1) return false;
-
-  if ((s = ccw(b, c, d)) && s > 0 === ccw(a, c, d) > 0) return false;
-  if ((s = ccw(a, b, d)) && s > 0 === ccw(a, b, c) > 0) return false;
-  return true;
+  return !(c0 > b0 || d0 < a0 || c1 > b1 || d1 < a1)
+      && !((s = ccw(b, c, d)) && s > 0 === ccw(a, c, d) > 0)
+      && !((s = ccw(a, b, d)) && s > 0 === ccw(a, b, c) > 0);
 }
