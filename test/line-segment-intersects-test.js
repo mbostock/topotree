516e765a428b5070d811d4a5b8c230b704e3b5a8
Mike Bostock <mbostock@gmail.com>
2014-10-08 10:55:44 -0700

    Break-out ccw test.

--- test/line-segment-intersects-test.js
+++ test/line-segment-intersects-test.js
@@ -201,48 +201,6 @@ suite.addBatch({
       assert.equal(lineSegment(2, 2, 0, 0).intersects(lineSegment(1, 0, 1, 1 + ε)), true);
       assert.equal(lineSegment(2, 2, 0, 0).intersects(lineSegment(1, 1 + ε, 1, 0)), true);
     }
-  },
-
-  "counterclockwise": {
-    topic: function() {
-      smash.load(["src/line-segment.js"], "lineSegment_ccw", {console: console}, this.callback);
-    },
-
-    "returns zero for three coincident points": function(ccw) {
-      assert.equal(ccw(0, 0, 0, 0, 0, 0), 0);
-      assert.equal(ccw(1, 1, 1, 1, 1, 1), 0);
-      assert.equal(ccw(2, 1, 2, 1, 2, 1), 0);
-      assert.equal(ccw(1, 2, 1, 2, 1, 2), 0);
-    },
-
-    "returns zero for three collinear points": function(ccw) {
-      assert.equal(ccw(0, 0, 0, 1, 0, 2), 0);
-      assert.equal(ccw(0, 0, 0, 2, 0, 1), 0);
-      assert.equal(ccw(0, 0, 2, 0, 1, 0), 0);
-      assert.equal(ccw(0, 0, 0, 0, 1, 0), 0);
-    },
-
-    //
-    // A---B
-    //  \  |
-    //   \ |
-    //    \|
-    //     C
-    //
-    "returns a value less than zero for a clockwise triangle": function(ccw) {
-      assert.ok(ccw(0, 0, 1, 0, 1, 1) < 0);
-    },
-
-    //
-    //     C
-    //    /|
-    //   / |
-    //  /  |
-    // A---B
-    //
-    "returns a value greater than zero for a counterclockwise triangle": function(ccw) {
-      assert.ok(ccw(0, 1, 1, 1, 1, 0) > 0);
-    }
   }
 });
 
