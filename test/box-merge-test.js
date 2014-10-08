faa0ab561099e5e5864d49bb088ad0e68afda769
Mike Bostock <mbostock@gmail.com>
2014-10-08 10:48:01 -0700

    Subsume test.

--- test/box-merge-test.js
+++ test/box-merge-test.js
@@ -9,30 +9,38 @@ var ε = 1e-6;
 suite.addBatch({
   "box.merge": {
     topic: function() {
-      smash.load(["src/line-segment.js"], "lineSegment", {console: console}, this.callback);
+      smash.load(["src/box.js"], "box", {console: console}, this.callback);
     },
 
-    "children is the two child nodes": function(lineSegment) {
-      var b0 = lineSegment(0, 0, 1, 1).box(),
-          b1 = lineSegment(1, 2, 3, 4).box();
+    "children is the two child nodes": function(box) {
+      var b0 = box(0, 0, 1, 1, []),
+          b1 = box(1, 2, 3, 4, []);
       assert.deepEqual([b0, b1], b0.merge(b1).children);
     },
 
-    "merges the bounding box of the two children": function(lineSegment) {
-      assert.deepEqual([0, 0, 3, 4], extent(lineSegment(0, 0, 1, 1).box().merge(lineSegment(1, 2, 3, 4).box())));
-      assert.deepEqual([0, 0, 3, 4], extent(lineSegment(1, 2, 3, 4).box().merge(lineSegment(0, 0, 1, 1).box())));
-      assert.deepEqual([0, 0, 3, 4], extent(lineSegment(0, 0, 3, 4).box().merge(lineSegment(1, 2, 1, 1).box())));
-      assert.deepEqual([0, 0, 3, 4], extent(lineSegment(1, 2, 1, 1).box().merge(lineSegment(0, 0, 3, 4).box())));
-      assert.deepEqual([0, 0, 3, 4], extent(lineSegment(0, 4, 3, 0).box().merge(lineSegment(1, 2, 1, 1).box())));
-      assert.deepEqual([0, 0, 3, 4], extent(lineSegment(1, 1, 1, 2).box().merge(lineSegment(0, 0, 3, 4).box())));
-      assert.deepEqual([0, 0, 3, 4], extent(lineSegment(0, 4, 3, 0).box().merge(lineSegment(1, 1, 1, 2).box())));
-      assert.deepEqual([0, 0, 3, 4], extent(lineSegment(1, 1, 1, 2).box().merge(lineSegment(0, 4, 3, 0).box())));
+    "merges the bounding box of the two children": function(box) {
+      assert.deepEqual([0, 0, 3, 4], extent(box(0, 0, 1, 1, []).merge(box(1, 2, 3, 4, []))));
+      assert.deepEqual([0, 0, 3, 4], extent(box(0, 0, 3, 4, []).merge(box(1, 1, 1, 2, []))));
+      assert.deepEqual([0, 0, 3, 4], extent(box(1, 2, 3, 4, []).merge(box(0, 0, 1, 1, []))));
+      assert.deepEqual([0, 0, 3, 4], extent(box(1, 1, 1, 2, []).merge(box(0, 0, 3, 4, []))));
+    },
+
+    "merges in-place when the bounding box of one subsumes the other": function(box) {
+      var b0 = box(0, 0, 3, 3, []),
+          b1 = box(1, 1, 2, 2, []);
+      assert.deepEqual(b0, b0.merge(b1));
+      assert.deepEqual([b1], b0.children);
+
+      var b0 = box(0, 0, 3, 3, []),
+          b1 = box(1, 1, 2, 2, []);
+      assert.deepEqual(b0, b1.merge(b0));
+      assert.deepEqual([b1], b0.children);
     }
   }
 });
 
-function extent(lineSegment) {
-  return [lineSegment.x0, lineSegment.y0, lineSegment.x1, lineSegment.y1];
+function extent(box) {
+  return [box.x0, box.y0, box.x1, box.y1];
 }
 
 suite.export(module);
